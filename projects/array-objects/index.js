4f99f4d9c3bb1419831bbc865e90270e7af5b93f
Ruslana Ilchuk <ilchukrusl@gmail.com>
2021-03-13 16:55:40 +0200

    homework_2 added

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,11 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
 
 /*
  Задание 2:
@@ -20,7 +24,25 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+
+function map(array, fn) {
+  const results = [];
+  for (let i = 0; i < array.length; i++) {
+    results[i] = fn(array[i], i, array);
+  }
+  return results;
+}
+
+// function map(array, fn) {
+
+//   let results = [];
+//   for (var i = 0; i<array.length; i++ ){
+
+//     results.push(fn(array[i], i, array));
+
+//   }
+//    return results
+// }
 
 /*
  Задание 3:
@@ -31,7 +53,31 @@ function map(array, fn) {}
  Пример:
    reduce([1, 2, 3], (all, current) => all + current) // 6
  */
-function reduce(array, fn, initial) {}
+
+// function reduce(array, fn, initial) {
+//   if (typeof initial !== undefined){
+//     var hasInitial = initial;
+//   }
+//   if (hasInitial){
+//     var prev = initial;
+//   }else{
+//     var prev = array[0];
+//   }
+//   for (var i = hasInitial ? 0:1; i<array.length; i++ ){
+//    prev = fn(prev, array[i], i, array);
+//   }
+//   return prev;
+// };
+
+function reduce(array, fn, initial) {
+  const hasInitial = typeof initial !== 'undefined';
+  let prev = hasInitial ? initial : array[0];
+
+  for (let i = hasInitial ? 0 : 1; i < array.length; i++) {
+    prev = fn(prev, array[i], i, array);
+  }
+  return prev;
+}
 
 /*
  Задание 4:
@@ -41,7 +87,15 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const result = [];
+  for (const name in obj) {
+    result.push(name.toUpperCase());
+  }
+  return result;
+
+  //return Object.keys(obj).map(name=>name.toUpperCase())
+}
 
 /*
  Задание 5 *:
@@ -54,6 +108,13 @@ function upperProps(obj) {}
    obj.foo = 2;
    console.log(obj.foo); // 4
  */
-function createProxy(obj) {}
+function createProxy(obj) {
+  return new Proxy(obj, {
+    set(obj, key, value) {
+      obj[key] = value ** 2;
+      return true;
+    },
+  });
+}
 
 export { forEach, map, reduce, upperProps, createProxy };
