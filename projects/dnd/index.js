39b668f9825829cb68d7cb931b2e09511807ac97
Ruslana Ilchuk <ilchukrusl@gmail.com>
2021-03-25 22:41:35 +0200

    div add func done

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,55 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+document.addEventListener('mousemove', (e) => {
+  //   var coords = getCoords(newDiv);
+  //   var shiftX = e.pageX - coords.left;
+  //   var shiftY = e.pageY - coords.top;
+  //   newDiv.style.position = 'absolute';
+  //   document.body.appendChild(newDiv);
+  //   moveAt(e);
+  //   newDiv.style.zIndex = 1000;
+  //   function moveAt(e) {
+  //     newDiv.style.left = e.pageX - shiftX + 'px';
+  //     newDiv.style.top = e.pageY - shiftY + 'px';
+  //   }
+  //   newDiv.onmouseup = function() {
+  //     document.onmousemove = null;
+  //     newDiv.onmouseup = null;
+  //   };
+  //   newDiv.ondragstart = function() {
+  //     return false;
+  //   };
+  //   function getCoords(elem) {
+  //     var box = elem.getBoundingClientRect();
+  //     return {
+  //       top: box.top + pageYOffset,
+  //       left: box.left + pageXOffset
+  //     };
+  //   }
+  // })
+});
 
-export function createDiv() {}
+export function createDiv() {
+  const div = document.createElement('div');
+  div.classList.add('draggable-div');
+  const divsize = (Math.random() * 100 + 50).toFixed();
+  const color =
+    '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
+
+  div.style.width = divsize + 'px';
+  div.style.height = divsize + 'px';
+  div.style.backgroundColor = color;
+
+  const posx = (Math.random() * (document.body.clientWidth - divsize)).toFixed();
+  const posy = (Math.random() * (document.body.clientHeight - divsize)).toFixed();
+
+  div.style.left = posx + 'px';
+  div.style.top = posy + 'px';
+  div.style.display = 'none';
+
+  return div;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
